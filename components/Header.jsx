d45b1ed5b570303f3c212782946916c4c87d2144
shahriaarrr <shahriaarrr@gmail.com>
2024-07-03 15:32:03 +0330

    fixed ui bug

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container mx-auto">header</div>
+    </header>
+  );
+};
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { Accessibility } = require('lucide-react');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -13,23 +11,23 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "15px",
+      padding: "2rem",
     },
     screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "960px",
-        xl: "1200px",
+      sm: '640px',
+      md: '760px',
+      lg: '980px',
+      xl: '1200px',
     },
     fontFamily: {
-      primary: "var(--font-jetbrainsMono)"
+      primary: "var(--font-jetbrainsMono)",
     },
     extend: {
       colors: {
         primary: "#1c1c22",
         accent: {
-          DEFAULT: '#00ff99',
-          hover: '#00e187',
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
         },
       },
       keyframes: {
