5edeb363438bc12d9a2a3a178e5a626e59761d59
shahriaarrr <shahriaarrr@gmail.com>
2024-07-05 22:15:51 +0330

    fixed homepage

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -54,7 +55,7 @@ const Home = () => {
         </div>
       </div>
 
-      
+      <Stats />
     </section>
   )
 }
